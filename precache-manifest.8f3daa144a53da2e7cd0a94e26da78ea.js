ff49bf2440c047b41057991ecb6f7eac4ac9314e
Parth Patel <parth2458@gmail.com>
2020-10-01 07:26:50 +0530

    Updates

---
+++ precache-manifest.8f3daa144a53da2e7cd0a94e26da78ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8197476c55186bcbb6a5ae685577f298",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "403b66a1e67528487322",
+    "url": "/Todo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f9ae2901e19df2f1d4a8",
+    "url": "/Todo/static/js/2.67957f51.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Todo/static/js/2.67957f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "403b66a1e67528487322",
+    "url": "/Todo/static/js/main.def4ba9b.chunk.js"
+  },
+  {
+    "revision": "c525ceef8cfd2f9caabe",
+    "url": "/Todo/static/js/runtime-main.aec79035.js"
+  }
+]);
